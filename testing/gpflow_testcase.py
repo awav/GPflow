5dde1b6b812f5b8fcea958471f4b9079ca59a8a5
Artem Artemev <art.art.v@gmail.com>
2017-08-07 22:01:36 +0100

    Revert change in RELEASE.md script

--- testing/gpflow_testcase.py
+++ testing/gpflow_testcase.py
@@ -6,6 +6,9 @@ class GPflowTestCase(unittest.TestCase):
     Wrapper for TestCase to avoid massive duplication of resetting
     Tensorflow Graph.
     """
+
+    _multiprocess_can_split_ = True
+
     def tearDown(self):
         tf.reset_default_graph()
         super(GPflowTestCase, self).tearDown()
